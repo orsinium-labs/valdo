245c5017374b959356df6fe9b8526003c264ea57
gram <git@orsinium.dev>
2024-11-12 17:54:08 +0100

    add properties

--- object.go
+++ object.go
@@ -19,19 +19,21 @@ func (obj Object) Validate(data any) Errors {
 }
 
 func (obj Object) Schema() jsony.Object {
-	res := jsony.Object{
-		jsony.Field{K: "type", V: jsony.SafeString("object")},
-	}
 	required := make(jsony.Array[jsony.String], 0)
+	properties := make(jsony.Map, 0)
 	for _, p := range obj.ps {
 		if !p.optional {
 			required = append(required, jsony.String(p.name))
 		}
+		properties[jsony.String(p.name)] = p.validator.Schema()
+	}
+	res := jsony.Object{
+		jsony.Field{K: "type", V: jsony.SafeString("object")},
+		jsony.Field{K: "properties", V: properties},
 	}
 	if len(required) != 0 {
 		res = append(res, jsony.Field{K: "required", V: required})
 	}
-	// TODO: add "properties"
 	return res
 }
 

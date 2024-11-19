6154e020a8eaeb9e2aa6563226045eba81096fc9
gram <git@orsinium.dev>
2024-11-19 15:09:54 +0100

    add AllOf

--- composers.go
+++ composers.go
@@ -2,6 +2,34 @@ package valdo
 
 import "github.com/orsinium-labs/jsony"
 
+type allOf struct {
+	vs []Validator
+}
+
+func AllOf(vs ...Validator) Validator {
+	return allOf{vs: vs}
+}
+
+func (n allOf) Validate(data any) error {
+	for _, v := range n.vs {
+		err := v.Validate(data)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (n allOf) Schema() jsony.Object {
+	ss := make(jsony.Array[jsony.Object], len(n.vs))
+	for i, v := range n.vs {
+		ss[i] = v.Schema()
+	}
+	return jsony.Object{
+		jsony.Field{K: "allOf", V: ss},
+	}
+}
+
 type notType struct {
 	v Validator
 }
